199d5815f898222e7ccacd3b60feca32db84de2b
Tomas Torres <axelagustom07@gmail.com>
2023-05-17 21:59:25 -0300

    Probando commit

--- my-app/src/Components/NavBar.js
+++ my-app/src/Components/NavBar.js
@@ -3,16 +3,7 @@ import { Container, Navbar, Nav } from "react-bootstrap";
 const NavBar = () => {
     return (
         <NavBar bg="white" variant="white">
-        <Container>
-          <Navbar.Brand href="#home">
-            Tienda de Productos
-          </Navbar.Brand>
-          <Nav ClassName="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Bebidas</Nav.Link>
-            <Nav.Link href="#pricing">Alimentos</Nav.Link>
-          </Nav>
-        </Container>
+        
       </NavBar>
     )
 }
